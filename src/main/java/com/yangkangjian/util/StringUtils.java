f361391c376e4fd171acf90371c5db2fc3fda510
scott <2567383143@qq.com>
2017-05-31 10:20:09 +0800

    project init

--- src/main/java/com/yangkangjian/util/StringUtils.java
+++ src/main/java/com/yangkangjian/util/StringUtils.java
@@ -5,10 +5,22 @@ package com.yangkangjian.util;
  * Created by scott on 2017/5/31.
  */
 public final class StringUtils {
+    public static final String EMPTY = "";
+    public static final String NULL = null;
+
     private StringUtils() {
     }
 
-    public static boolean isEmpty() {
-        return false;
+    public static boolean isNullOrEmpty(String string) {
+        return string == null || string.trim().isEmpty();
     }
+
+    public static String emptyToNull(String string) {
+        return isNullOrEmpty(string) ? null : string;
+    }
+
+    public static String nullToEmpty(String string) {
+        return (string == null) ? "" : string;
+    }
+
 }
--- src/test/java/com/yangkangjian/Test.java
+++ src/test/java/com/yangkangjian/Test.java
@@ -1,11 +1,16 @@
 package com.yangkangjian;
 
+import com.yangkangjian.util.StringUtils;
+
 /**
  * Created by scott on 2017/5/31.
  */
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println(StringUtils.isNullOrEmpty("\t"));
+        System.out.println(StringUtils.nullToEmpty(null).length());
+        System.out.println(StringUtils.emptyToNull(""));
     }
 
 }
