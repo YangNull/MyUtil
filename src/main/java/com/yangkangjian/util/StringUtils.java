3fdb4e8e93547061084b819c11a64dbb07619f5e
scott <2567383143@qq.com>
2017-05-31 09:23:44 +0800

    project init

---
+++ src/main/java/com/yangkangjian/util/StringUtils.java
@@ -0,0 +1,14 @@
+package com.yangkangjian.util;
+
+/**
+ * 字符串工具类
+ * Created by scott on 2017/5/31.
+ */
+public final class StringUtils {
+    private StringUtils() {
+    }
+
+    public static boolean isEmpty() {
+        return false;
+    }
+}
---
+++ src/test/java/com/yangkangjian/Test.java
@@ -0,0 +1,11 @@
+package com.yangkangjian;
+
+/**
+ * Created by scott on 2017/5/31.
+ */
+
+public class Test {
+    public static void main(String[] args) {
+    }
+
+}
